ccf3a7312cfbc576b9bc1024ef38fb1e8c7188b5
DhanielV <dafi0001@stud.kea.dk>
2024-05-03 11:44:10 +0200

    sortering refactor fra controller til egen class

--- src/Delfinen/KonkurrenceSvømmer.java
+++ src/Delfinen/KonkurrenceSvømmer.java
@@ -8,7 +8,6 @@ public class KonkurrenceSvømmer extends Medlem {
         super(navn, fødselsÅr, aktivitetsType, svømmeType, aldersType);
     }
 
-    //TODO: Lav en RandomGenerator, som betaler for medlemmer, hvis der står "HAR IKKE BETALT" 15 minutter efter programmet er startet
     @Override
     public void betalMedlemsGebyr() {
 
