5a8aa18bb7ad94dbf2353845d29b0f92007c7129
TheOriginalJiozx <omal0001@stud.kea.dk>
2024-05-17 12:16:47 +0200

    Opd

--- test/Delfinen/FormandTest.java
+++ test/Delfinen/FormandTest.java
@@ -1,35 +1,36 @@
 package Delfinen;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.time.LocalDate;
 
 class FormandTest {
+    private Svømmeklub svømmeklub;
+    private Formand formand;
+
+    @BeforeEach
+    void setUp() {
+        svømmeklub = new Svømmeklub();
+        formand = new Formand(svømmeklub);
+    }
 
     @Test
     void testTilføjMedlem() {
-        // Arrange
-        Svømmeklub svømmeklub = new Svømmeklub();
-        Formand formand = new Formand(svømmeklub);
         String navn = "Test Person";
         String datoString = "2000-01-01";
         String aktivitetsTyp = "AKTIV";
-        String svommeTyp = "motionist";
+        String svommeTyp = "MOTIONIST";
         String aldersTyp = "JUNIOR";
 
-        // Act
         formand.tilføjMedlem(navn, datoString, aktivitetsTyp, svommeTyp, aldersTyp);
 
-        // Assert
         assertEquals(1, svømmeklub.getMedlemmer().size(), "Medlem burde være tilføjet");
         Medlem medlem = svømmeklub.getMedlemmer().get(0);
         assertEquals(navn, medlem.getNavn(), "Navnet skal matche");
         assertEquals(LocalDate.parse(datoString), medlem.getFødselsÅr(), "Fødselsår skal matche");
-        assertEquals(AktivitetsType.AKTIV, medlem.getAktivitetsType(), "Aktivitetstype skal matche");
-        assertEquals(SvømmeType.MOTIONIST, medlem.getSvømmeType(), "Svømmetype skal matche");
-        assertEquals(AldersType.JUNIOR, medlem.getAldersType(), "Alderstype skal matche");
+        assertEquals(AktivitetsType.valueOf(aktivitetsTyp), medlem.getAktivitetsType(), "Aktivitetstype skal matche");
+        assertEquals(SvømmeType.valueOf(svommeTyp), medlem.getSvømmeType(), "Svømmetype skal matche");
+        assertEquals(AldersType.valueOf(aldersTyp), medlem.getAldersType(), "Alderstype skal matche");
     }
-
 }
